797e296bf9d268919c3e96c97a84a525856d8f71
RGhassen <rghassen@outlook.fr>
2023-06-06 23:20:05 +0100

    commit generated by  shell script

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def uppercase(s):
+    result = ""
+    for c in s:
+        if ord('a') <= ord(c) <= ord('z'):
+            result += "{}".format(chr(ord(c) - 32))
+        else:
+            result += "{}".format(c)
+    print(result)
