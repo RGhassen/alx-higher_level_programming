5f73b2ee01250fc4cb4f07ddcb64eab12cdea014
RGhassen <rghassen@outlook.fr>
2023-06-06 23:32:50 +0100

    commit generated by  shell script

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
-#101-remove_char_at.py
-
-
-def remove_char_at(astring, n):
-    if n < 0:
-        return (astring)
-    return (astring[:n] + astring[n+1:]) 
+def remove_char_at(str, n):
+    s = ""
+    for i in range(len(str)):
+        if i != n:
+            s = s + str[i]
+    return (s)
