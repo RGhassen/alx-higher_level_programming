fde81c79032942f1933ea34ef03f648953df20aa
RGhassen <rghassen@outlook.fr>
2023-06-06 23:23:14 +0100

    commit generated by  shell script

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-def print_last_digit(number):
-    last_digit = number % 10
-    print(last_digit, end='')
-    return last_digit
+# 9-print_last_digit.py
+
+
+def print_last_digit(anum):
+    print(abs(anum) % 10, end="")
+    return (abs(anum) % 10)
